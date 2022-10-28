e5cd7eb30aecb9f0611ec6e7cf5976a5410ad1d5
Alicia Zangger <alicia.zangger@gmail.com>
2022-10-28 15:40:06 +0200

    Create project

---
+++ test-ios/test_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_iosApp.swift
+//  test-ios
+//
+//  Created by AliciaZangger on 28.10.22.
+//
+
+import SwiftUI
+
+@main
+struct test_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
